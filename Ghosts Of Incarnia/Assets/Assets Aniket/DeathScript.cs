28d328675ebd89d8c1a041eba3759bad2d43be7d
aniket662 <deya7153@gmail.com>
2024-02-04 04:23:34 +0530

    added death ui

---
+++ Ghosts Of Incarnia/Assets/Assets Aniket/DeathScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DeathScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject GoBack;
+    public void DeathButton(){
+        GoBack.SetActive(true);
+    }
+    public void GoBackButton(){
+        SceneManager.LoadScene("UI");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
