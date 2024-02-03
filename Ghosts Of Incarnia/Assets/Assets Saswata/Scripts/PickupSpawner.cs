89d9174cf92b2521c57a4596840e1562af0dda99
aniket662 <deya7153@gmail.com>
2024-02-03 15:39:29 +0530

    Added Keys Pickup and Counter

--- Ghosts Of Incarnia/Assets/Assets Saswata/Scripts/PickupSpawner.cs
+++ Ghosts Of Incarnia/Assets/Assets Saswata/Scripts/PickupSpawner.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 
 public class PickupSpawner : MonoBehaviour
 {
-    [SerializeField]private GameObject HealthPickup;
-    public void DropItems(){
-        Instantiate(HealthPickup,transform.position,Quaternion.identity);
-    }    
+    [SerializeField] private GameObject HealthPickup;
+    [SerializeField] private GameObject KeyPickup;
+    
+
+    private int totalKeyPickups = 10; // Total number of KeyPickups
+    private int keyPickupCount = 0;
+    
+    public void DropItems()
+    {
+        if (keyPickupCount < totalKeyPickups && Random.Range(0f, 1f) < 0.2f)
+        {
+            // Spawn KeyPickup with a lower probability (0.2)
+            Instantiate(KeyPickup, transform.position, Quaternion.identity);
+            keyPickupCount++;
+        }
+        else
+        {
+            // Spawn HealthPickup with a higher probability (0.8)
+            Instantiate(HealthPickup,transform.position, Quaternion.identity);
+        }
+    }
+
+    
 }
