1800140e348cf17afe0f0cff01dc50f5aea51b1f
aniket662 <deya7153@gmail.com>
2024-02-03 16:53:34 +0530

    Fix(IG)

--- Ghosts Of Incarnia/Assets/Assets Saswata/Scripts/Player/Destructible.cs
+++ Ghosts Of Incarnia/Assets/Assets Saswata/Scripts/Player/Destructible.cs
@@ -4,12 +4,40 @@ using UnityEngine;
 
 public class Destructible : MonoBehaviour
 {
-    [SerializeField]GameObject destoryVFX;
-    private void OnTriggerEnter2D(Collider2D other){
-        if(other.gameObject.GetComponent<DamegeSource>()||other.gameObject.GetComponent<Projectile>()){
-            //GetComponent<PickupSpawner>().DropItems();
-            Instantiate(destoryVFX,transform.position,Quaternion.identity);
+    [SerializeField] GameObject destroyVFX;
+    [SerializeField] float explosionForce = 10f;
+    [SerializeField] float explosionRadius = 5f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<DamegeSource>() || other.gameObject.GetComponent<Projectile>())
+        {
+            // Instantiate explosion prefab
+            Instantiate(destroyVFX, transform.position, Quaternion.identity);
+
+            // Apply explosion force to nearby objects
+            ExplodeObjects();
+
+            // Destroy the destructible object
             Destroy(gameObject);
+        }
+    }
+
+    private void ExplodeObjects()
+    {
+        // Get all colliders within the explosion radius
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
+
+        foreach (Collider2D collider in colliders)
+        {
+            // Check if the object has a rigidbody2D
+            Rigidbody2D rb = collider.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                // Apply explosion force to the object
+                Vector2 direction = (rb.transform.position - transform.position).normalized;
+                rb.AddForce(direction * explosionForce, ForceMode2D.Impulse);
+            }
         }
     }
 }
